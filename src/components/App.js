886b4489df05bf6a957c95786cc1af1fd1709032
SakshiShreya <shreyasakshi96@gmail.com>
2019-10-06 02:14:50 +0530

    Add structure of keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { HashRouter } from 'react-router-dom';
-import './App.css';
+import OutputScreen from './OutputScreen';
+import Keypad from './Keypad';
+import '../styles/calculator.css';
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="App">
-          <h1> Hello, World! </h1>
+        <div className="app">
+          <OutputScreen historyText="test" output="test2" />
+          <Keypad />
         </div>
       </HashRouter>
     );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.js";
+import App from "./components/App";
+import './styles/light-theme.css';
+import './styles/layout.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
