ed824c87577efa228d76a1c69c79f056795a621f
SakshiShreya <shreyasakshi96@gmail.com>
2019-10-28 01:03:11 +0530

    Enter numbers in a better way

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,82 +1,54 @@
 import React, { Component } from 'react';
 
-class Keypad extends Component {
-  onKeyClick(key) {
-    return this.props.onKeyClick(key);
-  }
-
-  render() {
-    return (
-      <div className="keypad__container">
-        <div className="keypad__container__horizontal">
-          <div className="keypad__left-side">
-            <div className="keypad__row">
-              <button className="key">C</button>
-              <button className="key">+/-</button>
-              <button className="key">%</button>
-            </div>
-            <div className="keypad__row">
-              <button className="key" onClick={() => this.onKeyClick(7)}>
-                7
-              </button>
-              <button className="key" onClick={() => this.onKeyClick(8)}>
-                8
-              </button>
-              <button className="key" onClick={() => this.onKeyClick(9)}>
-                9
-              </button>
-            </div>
-            <div className="keypad__row">
-              <button className="key" onClick={() => this.onKeyClick(4)}>
-                4
-              </button>
-              <button className="key" onClick={() => this.onKeyClick(5)}>
-                5
-              </button>
-              <button className="key" onClick={() => this.onKeyClick(6)}>
-                6
-              </button>
-            </div>
-            <div className="keypad__row">
-              <button className="key" onClick={() => this.onKeyClick(1)}>
-                1
-              </button>
-              <button className="key" onClick={() => this.onKeyClick(2)}>
-                2
-              </button>
-              <button className="key" onClick={() => this.onKeyClick(3)}>
-                3
-              </button>
-            </div>
-            <div className="keypad__row">
-              <button className="key" onClick={() => this.onKeyClick(0)}>
-                0
-              </button>
-              <button className="key"></button>
-              <button className="key"></button>
-            </div>
-          </div>
-          <div className="keypad__right-side">
-            <div className="keypad__row">
-              <button className="key">/</button>
-            </div>
-            <div className="keypad__row">
-              <button className="key">x</button>
-            </div>
-            <div className="keypad__row">
-              <button className="key">-</button>
-            </div>
-            <div className="keypad__row">
-              <button className="key">+</button>
-            </div>
-          </div>
+const Keypad = props => (
+  <div className="keypad__container">
+    <div className="keypad__container__horizontal">
+      <div className="keypad__left-side">
+        <div className="keypad__row">
+          <input type="button" className="key" value="C" />
+          <input type="button" className="key" value="+/-" />
+          <input type="button" className="key" value="%" />
         </div>
-        <div className="keypad__equal-button">
-          <div className="equal-key">=</div>
+        <div className="keypad__row">
+          <input type="button" className="key" onClick={props.onKeyClick} value="7" />
+          <input type="button" className="key" onClick={props.onKeyClick} value="8" />
+          <input type="button" className="key" onClick={props.onKeyClick} value="9" />
+        </div>
+        <div className="keypad__row">
+          <input type="button" className="key" onClick={props.onKeyClick} value="4" />
+          <input type="button" className="key" onClick={props.onKeyClick} value="5" />
+          <input type="button" className="key" onClick={props.onKeyClick} value="6" />
+        </div>
+        <div className="keypad__row">
+          <input type="button" className="key" onClick={props.onKeyClick} value="1" />
+          <input type="button" className="key" onClick={props.onKeyClick} value="2" />
+          <input type="button" className="key" onClick={props.onKeyClick} value="3" />
+        </div>
+        <div className="keypad__row">
+          <input type="button" className="key" onClick={props.onKeyClick} value="0" />
+          <input type="button" className="key" />
+          <input type="button" className="key" />
         </div>
       </div>
-    );
-  }
-}
+      <div className="keypad__right-side">
+        <div className="keypad__row">
+          <input type="button" className="key" value="/" />
+        </div>
+        <div className="keypad__row">
+          <input type="button" className="key" value="x" />
+        </div>
+        <div className="keypad__row">
+          <input type="button" className="key" value="-" />
+        </div>
+        <div className="keypad__row">
+          <input type="button" className="key" value="+" />
+        </div>
+      </div>
+    </div>
+    <div className="keypad__equal-button">
+      <div className="equal-key">=</div>
+    </div>
+  </div>
+);
 
 export default Keypad;
